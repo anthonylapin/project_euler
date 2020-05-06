944ce040bc601b7f59ea56c09ecf27f31348ed13
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-05-06 11:18:38 +0300

    problem 5

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
 
             //Problem2.Solution();
 
-            Problem4.Solution();
+            Problem5.Solution();
         }
 
     }
@@ -153,5 +153,43 @@ namespace ProjectEuler
 
             Console.WriteLine($"Found the palindrome {palin}, which is made from {factors[0]} and {factors[1]}");
         }
+    }
+
+    class Problem5
+    {
+        /*2520 is the smallest number that can be divided by each of the numbers from 1 to 10
+         * without any remainder. What is the smallest positive number that
+         * is evenly divisible by all of the numbers from 1 to 20? */
+        public static void Solution()
+        {
+            int res = 1;
+
+            for (int i = 2; i <= 20; i++)
+            {
+                res = Lcm(res, i);
+            }
+            
+
+            Console.WriteLine(res);
+        }
+
+        private static int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                a %= b;
+                a ^= b;
+                b ^= a;
+                a ^= b;
+            }
+
+            return a;
+        }
+
+        private static int Lcm(int a, int b)
+        {
+            return a / Gcd(a, b) * b;
+        }
+
     }
 }
