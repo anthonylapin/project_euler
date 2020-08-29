4f82d92f0ce6c60e61dbfd692d5f8c2671496948
Anton <anthonylapin11@gmail.com>
2020-08-29 13:39:17 +0300

    problem 10 done

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(Problem9.Solution());
+            Console.WriteLine(Problem10.Solution());
         }
 
     }
@@ -359,6 +359,52 @@ namespace ProjectEuler
                 }
                 m++;
             }
+        }
+    }
+
+    class Problem10
+    {
+        // Find the sum of all the primes below two million.
+        public static int Solution()
+        {
+            int sum = 0;
+            for(int i = 2; i < 2000000; i++)
+            {
+                if(IsPrime(i))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if(number <= 1)
+            {
+                return false;
+            }
+            if (number == 2)
+            {
+                return true;
+            }
+            if(number % 2 == 0)
+            {
+                return false;
+            }
+
+            int counter = 3;
+            while(counter * counter < number)
+            {
+                if(number % counter == 0)
+                {
+                    return false;
+                }
+
+                counter++;
+            }
+            return true;
         }
     }
 
